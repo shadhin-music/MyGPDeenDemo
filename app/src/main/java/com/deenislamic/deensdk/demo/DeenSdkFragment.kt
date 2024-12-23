b4401cd2633b59f86f15972a2396cc60e6239617
Rabiul Hasan <rabiul.gakk@gmail.com>
2024-12-23 13:21:46 +0600

    - MyGP Deen SDK Demo APP

---
+++ app/src/main/java/com/deenislamic/deensdk/demo/DeenSdkFragment.kt
@@ -0,0 +1,51 @@
+package com.deenislamic.deensdk.demo
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.deenislamic.sdk.DeenSDKCallback
+import com.deenislamic.sdk.DeenSDKCore
+import com.deenislamic.sdk.views.gphome.GPHome
+
+class DeenSdkFragment : Fragment(), DeenSDKCallback {
+
+    private lateinit var  gphome: GPHome
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        DeenSDKCore.setupPermissionRequest(this)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val getview = inflater.inflate(R.layout.fragment_deen_sdk, container, false)
+
+        gphome = getview.findViewById(R.id.gphome)
+
+        return getview
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        gphome.initSDK(
+            context = requireContext(),
+            language = "en",
+            baseApiUrl = "https://api.deenislamic.com/api/",
+            baseServiceUrl = "https://services.deenislamic.com/api/",
+            callback = this
+        )
+    }
+
+    override fun DeenRequireToken(){
+        val token  = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJBcHBsaWNhdGlvbiI6IkRlZW4gSXNsYW0iLCJuYW1lIjoiODgwMTczODQzOTIzNiIsInJvbGUiOiJTREsiLCJuYmYiOjE3MzQ2MDI2OTQsImV4cCI6MTczNDY4OTA5NCwiaWF0IjoxNzM0NjAyNjk0fQ.9rLR_wAMObbKcg73cGFvu9E3Tk3Ws5c5BzHQM7lW9rsQsySGMkoiAofrup226eJTe34PF41tdekT9a-aBB9TSg"
+        DeenSDKCore.setToken(token)
+
+    }
+
+}
---
+++ app/src/main/java/com/deenislamic/deensdk/demo/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.deenislamic.deensdk.demo
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.deenislamic.sdk.DeenSDKCallback
+import com.deenislamic.sdk.DeenSDKCore
+import com.deenislamic.sdk.views.gphome.GPHome
+
+class MainActivity : AppCompatActivity(), DeenSDKCallback {
+
+    private lateinit var startBtn:Button
+    private lateinit var  gphome: GPHome
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        gphome = findViewById(R.id.gphome)
+
+        startBtn = findViewById(R.id.startBtn)
+
+        gphome.initSDK(
+            context = this,
+            language = "en",
+            baseApiUrl = "https://api.deenislamic.com/api/",
+            baseServiceUrl = "https://services.deenislamic.com/api/",
+            callback = this
+        )
+
+        startBtn.setOnClickListener {
+            gphome.visibility = View.GONE
+            startBtn.visibility = View.GONE
+            openFragment(DeenSdkFragment())
+        }
+
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment) // Replace 'fragment_container' with your container ID
+            .commit()
+    }
+
+    override fun DeenRequireToken(){
+        val token  = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJBcHBsaWNhdGlvbiI6IkRlZW4gSXNsYW0iLCJuYW1lIjoiODgwMTczODQzOTIzNiIsInJvbGUiOiJTREsiLCJuYmYiOjE3MzQ2MDI2OTQsImV4cCI6MTczNDY4OTA5NCwiaWF0IjoxNzM0NjAyNjk0fQ.9rLR_wAMObbKcg73cGFvu9E3Tk3Ws5c5BzHQM7lW9rsQsySGMkoiAofrup226eJTe34PF41tdekT9a-aBB9TSg"
+        DeenSDKCore.setToken(token)
+
+    }
+}
