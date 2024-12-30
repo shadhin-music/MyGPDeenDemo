c56547a045d90cd01727d0e20914a946d232b89b
Rabiul Hasan <rabiul.gakk@gmail.com>
2024-12-30 12:01:48 +0600

    - MyGP Deen SDK Demo APP - SDK 0.0.26 - Fixed expire token

--- app/src/main/java/com/deenislamic/deensdk/demo/DeenSdkFragment.kt
+++ app/src/main/java/com/deenislamic/deensdk/demo/DeenSdkFragment.kt
@@ -6,10 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.deenislamic.sdk.DeenSDKCallback
 import com.deenislamic.sdk.DeenSDKCore
 import com.deenislamic.sdk.utils.toast
 import com.deenislamic.sdk.views.gphome.GPHome
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class DeenSdkFragment : Fragment(), DeenSDKCallback {
 
@@ -56,10 +61,19 @@ class DeenSdkFragment : Fragment(), DeenSDKCallback {
 
     }
 
-    override fun DeenTokenExpired() {
+    override suspend fun DeenTokenExpired():String {
 
-        // get callback when api return 401
+        val token  = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJBcHBsaWNhdGlvbiI6IkRlZW4gSXNsYW0iLCJuYW1lIjoiODgwMTczODQzOTIzNiIsInJvbGUiOiJTREsiLCJuYmYiOjE3MzU0NjY3MzksImV4cCI6MTczNTU1MzEzOSwiaWF0IjoxNzM1NDY2NzM5fQ.hTo8P3p8KL3kQIiYxqUlM2E7MSSwbktK9wyp5BiZGz-kSTcCv8S1LZOvZudkeOBym5gwE_pHJ6K_172e3gdX4A"
 
+        lifecycleScope.launch(Dispatchers.Main) {
+            context?.toast("Token expired")
+
+        }
+
+        return withContext(Dispatchers.IO){
+            delay(500)
+            token
+        }
     }
 
 }
